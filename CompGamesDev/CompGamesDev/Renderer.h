22c42b119f61f4768866bc9e5669ff40ec48cdf4
tywhite123 <tywhitehead@hotmail.co.uk>
2018-02-09 18:46:45 +0000

    Added more projects for subsystems & created a renderer class with a heightmap rendering

---
+++ CompGamesDev/CompGamesDev/Renderer.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "../../nclgl/OGLRenderer.h"
+#include "../../nclgl/Camera.h"
+#include "../../nclgl/HeightMap.h"
+
+class Renderer : public OGLRenderer
+{
+public:
+	Renderer(Window &parent);
+	virtual ~Renderer();
+
+	virtual void RenderScene();
+	virtual void UpdateScene(float msec);
+
+protected:
+	HeightMap* heightMap;
+	Camera* camera;
+	//Mesh* sphere;
+};
+
