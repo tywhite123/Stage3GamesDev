e88fa43ef950a5c806761caae37632d2f6f68b5b
Tyler <tywhitehead@hotmail.co.uk>
2018-02-15 10:55:42 +0000

    Uni version update

--- CompGamesDev/CompGamesDev/main.cpp
+++ CompGamesDev/CompGamesDev/main.cpp
@@ -1,5 +1,29 @@
-#include "..\..\nclgl\Window.h"
+#include <nclgl\Window.h>
+#include "Renderer.h"
+
+/*int main() {
+	Window w("Test", 800, 600);
+
+
+}*/
 
 int main() {
-	Window w("Test", 800, 600);
+	Window w("Index Buffers!", 800, 600, false);
+	if (!w.HasInitialised()) {
+		return -1;
+	}
+
+	Renderer renderer(w);
+	if (!renderer.HasInitialised()) {
+		return -1;
+	}
+
+	w.LockMouseToWindow(true);
+	w.ShowOSPointer(false);
+
+	while (w.UpdateWindow() && !Window::GetKeyboard()->KeyDown(KEYBOARD_ESCAPE)) {
+		renderer.UpdateScene(w.GetTimer()->GetTimedMS());
+		renderer.RenderScene();
+	}
+	return 0;
 }
