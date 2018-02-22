553b771470ec355929b270d4018d302accb5afc6
Tyler <tywhitehead@hotmail.co.uk>
2018-02-22 12:18:11 +0000

    Cleaned some old commented out code in the main

--- CompGamesDev/CompGamesDev/main.cpp
+++ CompGamesDev/CompGamesDev/main.cpp
@@ -4,32 +4,7 @@
 #include <Physics\Physics.h>
 #include <Input\Input.h>
 
-/*int main() {
-	Window w("Test", 800, 600);
-
-
-}*/
-
 int main() {
-	/*Window w("Index Buffers!", 800, 600, false);
-	if (!w.HasInitialised()) {
-		return -1;
-	}
-
-	Renderer renderer(w);
-	if (!renderer.HasInitialised()) {
-		return -1;
-	}
-
-	Graphics graphics(w, renderer);
-
-	w.LockMouseToWindow(true);
-	w.ShowOSPointer(false);
-
-	while (w.UpdateWindow() && !Window::GetKeyboard()->KeyDown(KEYBOARD_ESCAPE)) {
-		renderer.UpdateScene(w.GetTimer()->GetTimedMS());
-		renderer.RenderScene();
-	}*/
 
 	Window w("CSC3224 - Game Engine", 1920, 1080);
 
